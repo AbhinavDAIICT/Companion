0b66672728b97cf39cf14e173b257b3fe3f909d1
AbhinavDAIICT <abhinavtripathi01@hotmail.com>
2015-06-10 22:43:49 +0530

    Basic UI plus methods to track movement
    Map control and UI at starting point. Logic to track coordinates every
    second and draw the path taken by the user.

---
+++ Companion/LocationOps.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Device.Location;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Devices.Geolocation;
+
+namespace Companion
+{
+    class LocationOps
+    {
+        //This method is directly taken from MSDN website - https://msdn.microsoft.com/en-us/library/windows/apps/jj735578(v=vs.105).aspx
+        public static GeoCoordinate ConvertGeocoordinate(Geocoordinate geocoordinate)
+        {
+            return new GeoCoordinate
+                (
+                geocoordinate.Latitude,
+                geocoordinate.Longitude,
+                geocoordinate.Altitude ?? Double.NaN,
+                geocoordinate.Accuracy,
+                geocoordinate.AltitudeAccuracy ?? Double.NaN,
+                geocoordinate.Speed ?? Double.NaN,
+                geocoordinate.Heading ?? Double.NaN
+                );
+        }
+    }
+
+    
+}
---
+++ Companion/MainPage.xaml.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using Companion.Resources;
+using Windows.Devices.Geolocation;
+using System.Device.Location;
+using System.Windows.Shapes;
+using System.Windows.Media;
+using Microsoft.Phone.Maps.Controls;
+using System.IO.IsolatedStorage;
+using System.Windows.Threading;
+
+namespace Companion
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        Geolocator geolocator = null;
+        private DispatcherTimer timer = new DispatcherTimer();
+        bool tracking = false;
+        private GeoCoordinateWatcher watcher = new GeoCoordinateWatcher(GeoPositionAccuracy.High);
+        private MapPolyline line;
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+            ShowMyLocationOnTheMap();
+            timer.Interval = TimeSpan.FromSeconds(1);
+            line = new MapPolyline();
+            line.StrokeColor = Colors.Blue;
+            line.StrokeThickness = 10;
+            myMap.MapElements.Add(line);
+            watcher.PositionChanged += Watcher_PositionChanged;
+          
+        }
+
+        private void Watcher_PositionChanged(object sender, GeoPositionChangedEventArgs<GeoCoordinate> e)
+        {
+            latitudeBlock.Text = e.Position.Location.Latitude.ToString("0.000000000000");
+            longitudeBlock.Text = e.Position.Location.Longitude.ToString("0.000000000000");            
+            var coord = new GeoCoordinate(e.Position.Location.Latitude, e.Position.Location.Longitude);
+            myMap.Center = coord;
+            line.Path.Add(coord);
+        }
+
+
+
+        private async void ShowMyLocationOnTheMap()
+        {
+            // Get my current location.
+            Geolocator myGeolocator = new Geolocator();
+            Geoposition myGeoposition = await myGeolocator.GetGeopositionAsync();
+            Geocoordinate myGeocoordinate = myGeoposition.Coordinate;
+            GeoCoordinate myGeoCoordinate =
+            LocationOps.ConvertGeocoordinate(myGeocoordinate);
+            this.myMap.Center = myGeoCoordinate;
+            this.myMap.ZoomLevel = 13;
+            Ellipse myCircle = new Ellipse();
+            myCircle.Fill = new SolidColorBrush(Colors.Blue);
+            myCircle.Height = 4;
+            myCircle.Width = 4;
+            myCircle.Opacity = 50;
+
+            // Create a MapOverlay to contain the circle.
+            MapOverlay myLocationOverlay = new MapOverlay();
+            myLocationOverlay.Content = myCircle;
+            myLocationOverlay.PositionOrigin = new Point(0.5, 0.5);
+            myLocationOverlay.GeoCoordinate = myGeoCoordinate;
+
+            // Create a MapLayer to contain the MapOverlay.
+            MapLayer myLocationLayer = new MapLayer();
+            myLocationLayer.Add(myLocationOverlay);
+
+            // Add the MapLayer to the Map.
+            myMap.Layers.Add(myLocationLayer);
+        }
+
+        private void TrackLocation_Click(object sender, RoutedEventArgs e)
+        {
+           
+            if (!tracking)
+            {
+                watcher.Start();
+                timer.Start();
+                tracking = true;
+                TrackLocationButton.Content = "stop tracking";
+            }
+            else
+            {
+                watcher.Stop();
+                timer.Stop(); 
+                tracking = false;
+                TrackLocationButton.Content = "track location";
+                statusBlock.Text = "stopped";
+            }
+        }
+
+
+        void geolocator_StatusChanged(Geolocator sender, StatusChangedEventArgs args)
+        {
+            string status = "";
+
+            switch (args.Status)
+            {
+                case PositionStatus.Disabled:
+                    // the application does not have the right capability or the location master switch is off
+                    status = "location is disabled in phone settings";
+                    break;
+                case PositionStatus.Initializing:
+                    // the geolocator started the tracking operation
+                    status = "initializing";
+                    break;
+                case PositionStatus.NoData:
+                    // the location service was not able to acquire the location
+                    status = "no data";
+                    break;
+                case PositionStatus.Ready:
+                    // the location service is generating geopositions as specified by the tracking parameters
+                    status = "ready";
+                    break;
+                case PositionStatus.NotAvailable:
+                    status = "not available";
+                    // not used in WindowsPhone, Windows desktop uses this value to signal that there is no hardware capable to acquire location information
+                    break;
+                case PositionStatus.NotInitialized:
+                    // the initial state of the geolocator, once the tracking operation is stopped by the user the geolocator moves back to this state
+
+                    break;
+            }
+
+            Dispatcher.BeginInvoke(() =>
+            {
+                statusBlock.Text = status;
+            });
+        }
+
+       
+
+        protected override void OnNavigatedTo(System.Windows.Navigation.NavigationEventArgs e)
+        {
+            if (IsolatedStorageSettings.ApplicationSettings.Contains("LocationConsent"))
+            {
+                // User has opted in or out of Location
+                return;
+            }
+            else
+            {
+                MessageBoxResult result =
+                    MessageBox.Show("This app accesses your phone's location. Is that ok?",
+                    "Location",
+                    MessageBoxButton.OKCancel);
+
+                if (result == MessageBoxResult.OK)
+                {
+                    IsolatedStorageSettings.ApplicationSettings["LocationConsent"] = true;
+                }
+                else
+                {
+                    IsolatedStorageSettings.ApplicationSettings["LocationConsent"] = false;
+                }
+
+                IsolatedStorageSettings.ApplicationSettings.Save();
+            }
+        }
+
+        // Sample code for building a localized ApplicationBar
+        //private void BuildLocalizedApplicationBar()
+        //{
+        //    // Set the page's ApplicationBar to a new instance of ApplicationBar.
+        //    ApplicationBar = new ApplicationBar();
+
+        //    // Create a new button and set the text value to the localized string from AppResources.
+        //    ApplicationBarIconButton appBarButton = new ApplicationBarIconButton(new Uri("/Assets/AppBar/appbar.add.rest.png", UriKind.Relative));
+        //    appBarButton.Text = AppResources.AppBarButtonText;
+        //    ApplicationBar.Buttons.Add(appBarButton);
+
+        //    // Create a new menu item with the localized string from AppResources.
+        //    ApplicationBarMenuItem appBarMenuItem = new ApplicationBarMenuItem(AppResources.AppBarMenuItemText);
+        //    ApplicationBar.MenuItems.Add(appBarMenuItem);
+        //}
+    }
+}
